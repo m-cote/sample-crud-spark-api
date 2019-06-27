13878a5fb88db3c7523045e301d4b59ec201ab29
Milan Kotykov <milankotykov@outlook.com>
2019-06-27 17:28:39 +0300

    moved persistence config file to db folder

--- src/main/java/app/util/JpaUtil.java
+++ src/main/java/app/util/JpaUtil.java
@@ -12,7 +12,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.io.FileInputStream;
+import java.net.URL;
 import java.sql.Statement;
+import java.util.Objects;
 import java.util.Properties;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -20,7 +22,17 @@ public class JpaUtil {
 
     private static final Logger log = LoggerFactory.getLogger(ErrorResponder.class);
 
-    private static final String PERSISTENCE_CONFIG = System.getenv().getOrDefault("PERSISTENCE_CONFIG", "config/persistence.properties");
+    private static final String PERSISTENCE_CONFIG;
+
+    static {
+        String persistenceConfig = System.getenv().get("PERSISTENCE_CONFIG");
+        if (persistenceConfig == null) {
+            URL resource = JpaUtil.class.getClassLoader().getResource("db/persistence.properties");
+            Objects.requireNonNull(resource, "Default persistence config file not found");
+            persistenceConfig = resource.getPath();
+        }
+        PERSISTENCE_CONFIG = persistenceConfig;
+    }
 
     private static volatile EntityManagerFactory EM_FACTORY;
     private static final String PERSISTENCE_UNIT_NAME = "hibernate-persistence";
